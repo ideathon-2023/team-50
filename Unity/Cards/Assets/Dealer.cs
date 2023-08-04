8dacdc8a0482054e077a02de260a3a6ad2ccd12e
= <ribhavaggarwal29@gmail.com>
2023-08-04 09:51:50 +0530

    upload
    initial unity files; with blender models done

---
+++ Unity/Cards/Assets/Dealer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dealer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shuffle(){
+        //int[] deck = Enumerable.Range(1,52).ToArray();
+        //int randomN = Random.Range(0,deck.Count);
+
+    }
+}
